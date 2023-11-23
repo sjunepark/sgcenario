c0df2786f6a5fecd2ac672bf1dcee57c87a7c1b7
June Park <junepark202012@gmail.com>
2023-11-24 01:00:22 +0900

    feat: output selection from Debugger

--- src/lib/lexical/LexicalDebugger/LexicalDebugger.svelte
+++ src/lib/lexical/LexicalDebugger/LexicalDebugger.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 	import { onDestroy, onMount } from 'svelte';
 	import { getEditor } from '$lib/lexical/LexicalComposer/LexicalComposerContext';
-	import { COMMAND_PRIORITY_HIGH } from 'lexical';
+	import {
+		$getSelection as _$getSelection,
+		$isRangeSelection as _$isRangeSelection,
+		$isNodeSelection as _$isNodeSelection,
+		COMMAND_PRIORITY_HIGH,
+		SELECTION_CHANGE_COMMAND
+	} from 'lexical';
 	import { browser, dev } from '$app/environment';
 	import { portal } from '$lib/utils/portal';
 	import { BugPlay } from 'lucide-svelte';
 	import { createPopover } from '@melt-ui/svelte';
 
 	// todo: show selection
-
-	const editor = getEditor();
-
-	let recentCommands: { type: string; payload: unknown }[] = [];
-	const MAX_SIZE = 10;
-	let unRegisterCommandListeners = new Set<() => void>();
 
 	let target: HTMLElement | null;
 
@@ -29,6 +29,16 @@
 		}
 	});
 
+	const editor = getEditor();
+
+	let recentCommands: { time: string; type: string; payload: unknown }[] = [];
+	const MAX_SIZE = 10;
+	let unRegisterCommandListeners = new Set<() => void>();
+	let currentSelection: {
+		type: string;
+		textContent: string;
+	} = { type: '', textContent: '' };
+
 	onMount(() => {
 		target = document.querySelector('#main');
 
@@ -36,14 +46,45 @@
 
 		for (const [command] of commands) {
 			console.log(`Registering ${JSON.stringify(command)}`);
+			unRegisterCommandListeners.add(
+				editor.registerCommand(
+					SELECTION_CHANGE_COMMAND,
+					() => {
+						const selection = _$getSelection();
+						const type =
+							selection === null
+								? 'null'
+								: _$isRangeSelection(selection)
+								  ? 'RangeSelection'
+								  : _$isNodeSelection(selection)
+								    ? 'NodeSelection'
+								    : 'UNKNOWN';
+						currentSelection = { type: type, textContent: selection?.getTextContent() ?? 'N/A' };
+						return false;
+					},
+					COMMAND_PRIORITY_HIGH
+				)
+			);
+
 			const unRegisterCommandListener = editor.registerCommand(
 				command,
 				(payload) => {
-					console.log(`${command.type}`);
 					if (recentCommands.length >= MAX_SIZE) {
 						recentCommands.shift();
 					}
-					recentCommands = [...recentCommands, { type: command.type ?? 'UNKNOWN', payload }];
+					recentCommands = [
+						...recentCommands,
+						{
+							time: Intl.DateTimeFormat('default', {
+								hour: '2-digit',
+								minute: '2-digit',
+								second: '2-digit',
+								hour12: false
+							}).format(Date.now()),
+							type: command.type ?? 'UNKNOWN',
+							payload
+						}
+					];
 					return false;
 				},
 				COMMAND_PRIORITY_HIGH
@@ -63,13 +104,14 @@
 			<BugPlay />
 		</button>
 		{#if $open}
-			<div {...$content} use:content class="bg-white/90 p-2 text-sm ring-1 ring-gray-500">
+			<div {...$content} use:content class="max-w-2xl bg-white/90 p-2 text-xs ring-1 ring-gray-500">
 				<h2 class="text-base">Debugger</h2>
-				{#each recentCommands as command, index (index)}
-					<div class="command">
-						{JSON.stringify(command, null, 2)}
-					</div>
-				{/each}
+				<pre>{#each recentCommands as command, index (index)}{JSON.stringify(
+							{ ...command },
+							null
+						)}<br />{/each}</pre>
+				<h3 class="text-base">Selection</h3>
+				<pre>{JSON.stringify(currentSelection, null)}</pre>
 			</div>
 		{/if}
 	</div>
